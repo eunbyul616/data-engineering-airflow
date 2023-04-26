35ae0ea7b335a016638b7f85bfe964991f574480
eunbyul <eunbyul616@gmail.com>
2023-04-27 00:28:29 +0900

    fix: typing 오류 수정
    get_Redshift_connection 함수에서 postgres_conn_id 타이핑 오류 수정

--- dags/NameGenderCSVtoRedshift.py
+++ dags/NameGenderCSVtoRedshift.py
@@ -10,7 +10,7 @@ import logging
 import psycopg2
 
 def get_Redshift_connection(autocommit=False):
-    hook = PostgresHook(postgre_conn_id='redshift_dev_db')
+    hook = PostgresHook(postgres_conn_id='redshift_dev_db')
     conn = hook.get_conn()
     conn.autocommit = autocommit
     return conn.cursor()
